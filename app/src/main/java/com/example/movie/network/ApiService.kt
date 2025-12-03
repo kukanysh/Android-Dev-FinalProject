2fb7a6f83a7e64fb85edc2f5eff1ac98b69dfe45
Kuanysh <260505kuka@gmail.com>
2025-12-03 18:10:09 +0500

    [feature] Edited ApiService and added Gradle implementations

--- app/src/main/java/com/example/movie/network/ApiService.kt
+++ app/src/main/java/com/example/movie/network/ApiService.kt
@@ -1,8 +1,17 @@
 package com.example.movie.network
 
+import com.example.movie.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
-    @GET()
+    @GET("/")
+    suspend fun getMovies(
+        @Query("apikey") apiKey: String,
+        @Query("t") title: String
+    ): MovieResponse
+
 
 
 }
