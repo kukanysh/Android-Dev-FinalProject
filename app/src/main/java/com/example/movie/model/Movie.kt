9951f7cdd00bdd10ef0b1b2a1d54edc37600f6dd
Kuanysh <260505kuka@gmail.com>
2025-12-03 18:06:17 +0500

    [feature] Edited Movie model

--- app/src/main/java/com/example/movie/model/Movie.kt
+++ app/src/main/java/com/example/movie/model/Movie.kt
@@ -1,4 +1,19 @@
 package com.example.movie.model
 
-class Movie {
-}
+import com.google.gson.annotations.SerializedName
+data class Movie(
+    @SerializedName("Title") val title: String,
+    @SerializedName("Year") val year: String,
+    @SerializedName("Rated") val rated: String,
+    @SerializedName("Released") val released: String,
+    @SerializedName("Runtime") val runtime: String,
+    @SerializedName("Genre") val genre: String,
+    @SerializedName("Director") val director: String,
+    @SerializedName("Writer") val writer: String,
+    @SerializedName("Actors") val actors: String,
+    @SerializedName("Plot") val plot: String,
+    @SerializedName("Language") val language: String,
+    @SerializedName("Country") val country: String,
+    @SerializedName("Poster") val poster: String,
+    @SerializedName("imdbRating") val imdbRating: String,
+)
