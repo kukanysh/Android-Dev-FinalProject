dc80ee99f662f6721823a28aaf45e55a75d09695
Kuanysh <260505kuka@gmail.com>
2025-12-03 18:15:15 +0500

    [feature] Created and Edited RetrofitInstance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,15 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+// Kotlin coroutines for background execution
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+// RecyclerView (only if you display a list)
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+// Material Components (for modern UI widgets)
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("com.squareup.picasso:picasso:2.8")
+
 }
---
+++ app/src/main/java/com/example/movie/network/RetrofitInstance.kt
@@ -0,0 +1,30 @@
+package com.example.movie.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitInstance {
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private const val BASE_URL = "https://www.omdbapi.com/"
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(ApiService::class.java)
+    }
+
+}
