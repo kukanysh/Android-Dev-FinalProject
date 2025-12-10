2655f33d53a80202eabb91295cf17d8af8f1e881
Kuanysh <260505kuka@gmail.com>
2025-12-11 00:48:09 +0500

    [feature] Added Extension

---
+++ app/src/main/java/com/example/movie/room/MovieExtension.kt
@@ -0,0 +1,44 @@
+package com.example.movie.room
+
+import com.example.movie.model.Movie
+
+fun Movie.toEntity(): MovieEntity {
+    return MovieEntity(
+        imdbID = this.imdbID,
+        title = this.title,
+        year = this.year,
+        type = this.type,
+        poster = this.poster,
+        rated = this.rated,
+        released = this.released,
+        runtime = this.runtime,
+        genre = this.genre,
+        director = this.director,
+        writer = this.writer,
+        actors = this.actors,
+        plot = this.plot,
+        language = this.language,
+        country = this.country
+    )
+}
+
+fun MovieEntity.toMovie(): Movie {
+    return Movie(
+        imdbID = this.imdbID,
+        title = this.title,
+        year = this.year,
+        type = this.type,
+        poster = this.poster,
+        rated = this.rated,
+        released = this.released,
+        runtime = this.runtime,
+        genre = this.genre,
+        director = this.director,
+        writer = this.writer,
+        actors = this.actors,
+        plot = this.plot,
+        language = this.language,
+        country = this.country
+    )
+}
+
