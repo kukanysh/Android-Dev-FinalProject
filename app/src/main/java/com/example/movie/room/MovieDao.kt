ac8ae6f09243a71f254cd1a585d54130a2aed8b6
Kuanysh <260505kuka@gmail.com>
2025-12-11 00:47:33 +0500

    [fix] Edited

--- app/src/main/java/com/example/movie/room/MovieDao.kt
+++ app/src/main/java/com/example/movie/room/MovieDao.kt
@@ -4,17 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovies(movieList: List<MovieEntity>)
+    suspend fun insertMovies(movies: List<MovieEntity>)
 
     @Query("SELECT * FROM movies")
     suspend fun getAllMovies(): List<MovieEntity>
 
     @Query("SELECT * FROM movies WHERE imdbID = :id")
-    suspend fun getMovieById(id: String): Flow<MovieEntity?>
+    suspend fun getMovieById(id: String): MovieEntity?
 }
