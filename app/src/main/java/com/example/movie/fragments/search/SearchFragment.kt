3796e45f994f81b4792cb0ef6e87b7280b511fb3
Ser1kbay <serkbayaza@gmail.com>
2025-12-11 01:45:08 +0500

    [feature] added new details

--- app/src/main/java/com/example/movie/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/example/movie/fragments/search/SearchFragment.kt
@@ -1,7 +1,15 @@
 package com.example.movie.fragments.search
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.movie.R
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO: Add search functionality
+    }
 }
