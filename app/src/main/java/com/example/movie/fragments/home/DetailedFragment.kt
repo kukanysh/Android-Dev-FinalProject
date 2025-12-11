205f13f8f1653142ab614a6bd731c8934d24244a
Kuanysh <260505kuka@gmail.com>
2025-12-11 12:49:57 +0500

    [fix] edited detailed fragment

--- app/src/main/java/com/example/movie/fragments/home/DetailedFragment.kt
+++ app/src/main/java/com/example/movie/fragments/home/DetailedFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.movie.R
 import com.example.movie.network.RetrofitInstance
@@ -114,6 +116,11 @@ class DetailedFragment : Fragment(R.layout.detailed_fragment) {
                 startActivity(Intent.createChooser(shareIntent, "Share via"))
             }
         }
+
+        view.findViewById<ImageButton>(R.id.btnBack).setOnClickListener {
+            findNavController().popBackStack()
+        }
+
 
         viewModel.movieDetail.observe(viewLifecycleOwner) { movie ->
             title.text = movie?.title
